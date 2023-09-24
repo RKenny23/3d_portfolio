6c5f001fe0748579190aa66977bffd700f663959
RKenny23 <rkenny238@hotmail.com>
2023-09-23 21:18:31 -0400

    added space

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I&apos;m <span className="text-[#915eff]">Ryan</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals and
+            I develop 3D visuals and&nbsp;
             <br className="sm:block hidden" />
             explore hidden paths in the world of AI.
           </p>
